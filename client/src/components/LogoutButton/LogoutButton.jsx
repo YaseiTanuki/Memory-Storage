b674c3a0f4c03514c3194f7ee820bfa6a1932f08
タヌキ <Tanuki1707@gmail.com>
2023-07-29 20:30:54 +0700

    add Loggout button

---
+++ client/src/components/LogoutButton/LogoutButton.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useContext} from 'react'
+import AuthContext from "../../hook/useContext/authContext";
+
+export default function LogoutButton() {
+
+    const {setAuth} = useContext(AuthContext);
+    const Navigate = useNavigate();
+
+    const Logout = async(event) => {
+
+        const res = await fetch('http://localhost:1707/api/logout', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify ({
+                message : "Logout please"
+            })
+        });
+
+        if(res.status == 200){
+            setAuth({});
+            Navigate("/")
+        }
+    }
+
+    return(
+        <button onClick={Logout}>Logout</button>
+    )
+}
