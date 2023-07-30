7b2b90944ed019e89c28492701a9fa5dac46fa07
タヌキ <Tanuki1707@gmail.com>
2023-07-30 15:24:27 +0700

    use Album Context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './hook/useContext/authContext.jsx'
+import { AuthProvider } from './hooks/useContext/authContext.jsx'
+import { AlbumProvider } from './hooks/useContext/albumContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <AlbumProvider>
+          <App />
+        </AlbumProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
