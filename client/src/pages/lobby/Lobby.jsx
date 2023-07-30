1fd29d381b4bd1152879a1e6f9fd07af95531271
タヌキ <Tanuki1707@gmail.com>
2023-07-30 20:55:49 +0700

    change 'class' to 'className'

--- client/src/pages/lobby/Lobby.jsx
+++ client/src/pages/lobby/Lobby.jsx
@@ -2,7 +2,7 @@ import { RegLogButton } from '../../components/RegLogButton/RegLogButton'
 import { SiteName } from '../../components/SiteName/SiteName'
 export default function Lobby() {
     return (
-    <div class = "container">
+    <div className = "container">
         <SiteName/>
         <RegLogButton/>
     </div>
