8af6b9b11ab5af4c1533cfdf8644c4331c2aed9a
タヌキ <Tanuki1707@gmail.com>
2023-07-30 20:56:25 +0700

    add feature to products page

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,13 +1,54 @@
 import Product from "../../components/Product/Product"
 import './ProductsStyle.css'
+import AddProductForm from "../../components/Product/AddProductForm/AddProductForm"
+import AuthContext from "../../hooks/useContext/authContext"
+import ProductContext from "../../hooks/useContext/productContext"
+import { useContext, useState, useEffect } from "react"
 
 export default function Products() {
+    const {auth} = useContext(AuthContext)
+    const {allProducts, setAllProduct} = useContext(ProductContext)
+
+    const [products, setProducts] = useState([{}])
+
+    useEffect(() => {
+        async function TakeProducts() {
+          const OwnerName = auth.UserName;
+          const res = await fetch(("http://localhost:1707/api/home/product/" + OwnerName), {
+            method: 'GET',
+            headers: {
+              'Content-type': 'application/json'
+          }
+          })
+      
+          if(res.status == 200){
+            await res.json().then((data) => {
+              setProducts((products) => (data.data))
+              console.log(products)
+            })
+          }
+        }
+    
+        TakeProducts();
+      }, [allProducts])
+
     return (
     <div className="products">
         <ul>
-            <li><Product name="Product 1">First</Product></li>
-            <li><Product name="Product 2">Second</Product></li>
-            <li><Product name="Product 3">Third</Product></li>
+            <li><AddProductForm/></li>
+            {
+              products.map((product) => {
+                if(product.Name){
+                    return (
+                        <li><Product name={product.Name} >{product.Description}</Product></li>
+                )}
+                else{
+                    return (
+                        ""
+                    )
+                }
+              })
+            }
         </ul>
     </div>
     )
