c610267d27dbedb7f0bdbe2e33d245b67ce0cd41
タヌキ <Tanuki1707@gmail.com>
2023-07-30 22:48:12 +0700

    add profile button

--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -2,18 +2,23 @@ import { Link } from "react-router-dom"
 import './NBStyle.css'
 import AuthContext from "../../hooks/useContext/authContext"
 import { useContext } from "react"
-import LogoutButton from '../LogoutButton/LogoutButton'
+import Profile from "../Profile/Profile"
+import Popup from "reactjs-popup"
 
 export default function Navbar() {
   const {auth} = useContext(AuthContext);
   const MenuSelector = () => {
     if(auth.UserName) {
       return (
-        <ul>
-          <li><Link to="/album"><h2>Album</h2></Link></li>
-          <li><Link to="/product"><h2>Product</h2></Link></li>
-          <li><LogoutButton/></li>
-        </ul>
+        <div className="NavContainer">
+          <ul>
+            <li><Link to="/album"><h2>Album</h2></Link></li>
+            <li><Link to="/product"><h2>Product</h2></Link></li>
+          </ul>
+          <Popup trigger={<img className="userIcon" src="/img/userIcon.png"/>}>
+            <Profile/>
+          </Popup>
+        </div>
       )
     } else {
       return (
