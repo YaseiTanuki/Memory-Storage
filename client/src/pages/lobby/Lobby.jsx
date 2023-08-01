faf53e369d8c51eb8b6a63fb99c0e27fb3aead12
タヌキ <Tanuki1707@gmail.com>
2023-08-01 19:40:50 +0700

    lobby now will navigate to home if token is in storage

--- client/src/pages/lobby/Lobby.jsx
+++ client/src/pages/lobby/Lobby.jsx
@@ -1,6 +1,17 @@
+import { useContext } from 'react'
 import { RegLogButton } from '../../components/RegLogButton/RegLogButton'
 import { SiteName } from '../../components/SiteName/SiteName'
+import AuthContext from '../../hooks/useContext/authContext'
+import { Navigate } from 'react-router-dom'
+
 export default function Lobby() {
+    const {auth} = useContext(AuthContext)
+    if(auth.UserName) {
+        return (
+            <Navigate to="/home" replace/>
+        )
+    }
+
     return (
     <div className = "container">
         <SiteName/>
