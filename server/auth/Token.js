e428d3b7ad1c91381a28a1940c13913c84a01412
タヌキ <Tanuki1707@gmail.com>
2023-08-02 14:27:12 +0700

    update token expire time

--- server/auth/Token.js
+++ server/auth/Token.js
@@ -2,6 +2,6 @@ const jwt = require('jsonwebtoken')
 
 module.exports = {
     GenerateToken: (data) => {
-        return jwt.sign(data, process.env.TOKEN_SECRET, {expiresIn: '3600s'});
+        return jwt.sign(data, process.env.TOKEN_SECRET, {expiresIn: '24h'});
     }
 }
