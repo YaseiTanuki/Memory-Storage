4b76b9d9ad69f895a3479f41980bdd759380c0eb
タヌキ <Tanuki1707@gmail.com>
2023-08-02 14:28:59 +0700

    update Authorization method

--- client/src/components/RoutesAuth/RoutesAuth.jsx
+++ client/src/components/RoutesAuth/RoutesAuth.jsx
@@ -1,9 +1,28 @@
 import {Outlet, Navigate} from 'react-router-dom'
 import AuthContext from '../../hooks/useContext/authContext'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import useAuthAxios from '../../hooks/useAxios/useAuthAxios'
 
 const RoutesAuth = () => {
-    const {auth} = useContext(AuthContext)
+    const {auth, setAuth} = useContext(AuthContext)
+
+    useEffect(() => {
+        async function Authorize() {
+          const authAxios = useAuthAxios();
+          try {
+            await authAxios.get("/api/home").then((res) => {
+              console.log(res.data.message)
+            })
+          } catch (error) {
+            console.log("Unauthorized")
+            window.localStorage.clear();
+            setAuth({})
+          }
+        }
+    
+        Authorize();
+    })
+
     if(auth.UserName){
         return (
             <Outlet/>
