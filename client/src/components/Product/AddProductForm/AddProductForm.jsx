4d37d9948993d23f6c12e8f1f9b922068711a41f
タヌキ <Tanuki1707@gmail.com>
2023-08-03 21:58:04 +0700

    fix issue with axios

--- client/src/components/Product/AddProductForm/AddProductForm.jsx
+++ client/src/components/Product/AddProductForm/AddProductForm.jsx
@@ -1,11 +1,10 @@
 import { useContext, useState } from 'react';
-import AuthContext from '../../../hooks/useContext/authContext';
 import ProductContext from '../../../hooks/useContext/productContext';
 import useAuthAxios from '../../../hooks/useAxios/useAuthAxios';
+import './AddProductFormStyle.css'
 
 export default function AddProductForm() {
 
-    const {auth} = useContext(AuthContext)
     const {allProducts, setAllProducts} = useContext(ProductContext)
 
     const[product, setProduct] = useState({
@@ -20,20 +19,22 @@ export default function AddProductForm() {
         await authAxios.post("/api/home/product", {
             Name, Description
         }).then((res) => {
-            if(res.status == 200){
-                    console.log(res.data.message);
+            if(res.data.status == "OK"){
+                console.log(res.data.message);
                 setAllProducts(allProducts + 1)
             }
         })
     }
 
     return (
-        <form className='AddProductForm' action="">
+        <div className="AddProductForm">
+        <form action="">
             <label htmlFor="Name">Product name:</label><br />
             <input type="text" name="Name" id="" onChange={(event) => {setProduct({...product, Name: event.target.value})}}/><br />
             <label htmlFor="Description">Description</label><br />
             <textarea name="Description" id="" cols="30" rows="10" onChange={(event) => {setProduct({...product, Description: event.target.value})}}></textarea><br />
-            <input type="submit" onClick={Upload}/><br />
+            <input type="submit" onClick={Upload} value="Upload"/><br />
         </form>
+        </div>
     )
 }
--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,33 +1,30 @@
 import './ProductStyle.css'
 import ProductImg from '../../assets/img/prod.png'
 import Popup from 'reactjs-popup'
-import AuthContext from '../../hooks/useContext/authContext'
 import { useContext } from 'react'
 import ProductContext from '../../hooks/useContext/productContext'
 import useAuthAxios from '../../hooks/useAxios/useAuthAxios'
 
 export default function Product(props) {
 
-    const {auth} = useContext(AuthContext)
     const {allProducts, setAllProducts} = useContext(ProductContext)
     const Delete = async() => {
         const Name = props.name
-        console.log(Name)
         const authAxios = useAuthAxios();
         await authAxios.delete("/api/home/product", {
             data: {Name}
         }).then((res) => {
-            if(res.status == 200) {
-                console.log(res.data.message)
+            if(res.data.status == "OK") {
                 setAllProducts(allProducts - 1)
             }
+            console.log(res.data.message)
         })
     }
     return(<div className="product">
             <h2 className="name">{props.name}</h2>
             <img className='prodimg' src={ProductImg} alt="" />
             <p className="description">{props.children}</p>
-            <Popup trigger={<button>...</button>}>
+            <Popup trigger={<button className='more'>...</button>}>
                 <button onClick={Delete}>Delete</button>
             </Popup>
     </div>
