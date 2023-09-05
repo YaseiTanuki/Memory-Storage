d0e8f58fb838d455384b71a938aa1da7253a1674
タヌキ <Tanuki1707@gmail.com>
2023-09-05 21:16:26 +0700

    add more guild to guild page

--- client/src/components/Book/GuildPage.jsx
+++ client/src/components/Book/GuildPage.jsx
@@ -8,9 +8,11 @@ const GuildPage = React.forwardRef((props, ref) => {
     return (
         <div className="GuildPage" ref={ref}>
             <h2>Feature</h2>
-            <ul>
-                <li><p>1. You can upload your image with upload popup on left side.</p></li>
-            </ul>
+            <ol>
+                <li value={1}>You can upload your image with upload popup on left side.</li>
+                <li>Find your image by giving its title.</li>
+                <li>Delete image by clicking ... button - Delete</li>
+            </ol>
         </div>
     )
 })
